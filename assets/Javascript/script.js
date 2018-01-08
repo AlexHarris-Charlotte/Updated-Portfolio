73c0515164554a6c993ca06018a0b2f9e998f8fb
Alex Harris <akharri8@ncsu.edu>
2018-01-07 20:27:59 -0500

    update

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,24 +1,24 @@
 
-
-
-// Project Image Carousel
-
 var start = 0;
 var carouselButtonRight = document.getElementById("rightButton");
 var carouselButtonLeft = document.getElementById("leftButton");
 var imageCarousel = document.getElementById("imageCarousel");
 var projectLink = document.getElementById("projectLinks");
 var projectName = document.getElementById("projectName");
+var playButton = document.getElementById("playLink");
+
 // project constructor
-function project(name, image, link){
+function project(name, image, link, pageLink){
     this.name = name;
     this.image = image;
     this.link = link;
+    this.pageLink = pageLink;
 }
+
 // project objects
-var pong = new project("Pong", "assets/Images/pong.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/Pong");
-var spaceInvaders = new project("Space Invaders", "assets/Images/spaceInvaders.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/Space-Invaders");
-var jqueryCalc = new project("jQuery Calculator", "assets/Images/jqueryCalculator.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/jQuery-Calculator");
+var pong = new project("Pong", "assets/Images/pong.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/Pong", "https://alexharris-charlottecodecamp.github.io/Pong/");
+var spaceInvaders = new project("Space Invaders", "assets/Images/spaceInvaders.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/Space-Invaders", "https://alexharris-charlottecodecamp.github.io/Space-Invaders/");
+var jqueryCalc = new project("jQuery Calculator", "assets/Images/jqueryCalculator.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/jQuery-Calculator", "https://alexharris-charlottecodecamp.github.io/jQuery-Calculator/");
 
 var projectObjectArray = [jqueryCalc, pong, spaceInvaders];
 projectName.textContent = projectObjectArray[start].name;
@@ -27,30 +27,37 @@ projectName.textContent = projectObjectArray[start].name;
 carouselButtonRight.addEventListener("click", function(){
     if(start < 2) {
         start++;
-        imageCarousel.setAttribute("src", projectObjectArray[start].image);
-        projectLink.setAttribute("href", projectObjectArray[start].link);
-        projectName.textContent = projectObjectArray[start].name;
+        ClickFrom0();
     } else {
         start = 0;
-        imageCarousel.setAttribute("src", projectObjectArray[start].image);
-        projectLink.setAttribute("href", projectObjectArray[start].link);
-        projectName.textContent = projectObjectArray[start].name;
+        CarouselReset()
     }
 })
 // Activate click for left carousel button
 carouselButtonLeft.addEventListener("click", function(){
     if(start === 0) {
         start = projectObjectArray.length - 1;
-        imageCarousel.setAttribute("src", projectObjectArray[start].image);
-        projectLink.setAttribute("href", projectObjectArray[start].link);
-        projectName.textContent = projectObjectArray[start].name;
+        ClickFrom0();
     } else {
         start--;
-        imageCarousel.setAttribute("src", projectObjectArray[start].image);
-        projectLink.setAttribute("href", projectObjectArray[start].link);
-        projectName.textContent = projectObjectArray[start].name;
+        CarouselReset()
     }
 })
 
+function ClickFrom0() {
+    imageCarousel.setAttribute("src", projectObjectArray[start].image);
+    projectLink.setAttribute("href", projectObjectArray[start].link);
+    projectName.textContent = projectObjectArray[start].name;
+    playButton.setAttribute("href", projectObjectArray[start].pageLink);
+}
 
-// write code to change text content to display what the projects are to the user!!
+function CarouselReset() {
+    imageCarousel.setAttribute("src", projectObjectArray[start].image);
+    projectLink.setAttribute("href", projectObjectArray[start].link);
+    projectName.textContent = projectObjectArray[start].name;
+    playButton.setAttribute("href", projectObjectArray[start].pageLink);
+}
+
+
+
+
