fbe181c698bd7cf77e9cc431049a1c962cce350a
Alex Harris <akharri8@ncsu.edu>
2018-01-07 19:10:02 -0500

    added project carousel

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -7,23 +7,40 @@ var start = 0;
 var carouselButtonRight = document.getElementById("rightButton");
 var carouselButtonLeft = document.getElementById("leftButton");
 var imageCarousel = document.getElementById("imageCarousel");
-var imageArray = ["assets/Images/jqueryCalculator.jpg", "assets/Images/pong.jpg", "assets/Images/spaceInvaders.jpg"];
+var projectLink = document.getElementById("projectLinks");
+// project constructor
+function project(name, image, link){
+    this.name = name;
+    this.image = image;
+    this.link = link;
+}
+// project objects
+var pong = new project("Pong", "assets/Images/pong.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/Pong");
+var spaceInvaders = new project("Space Invaders", "assets/Images/spaceInvaders.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/Space-Invaders");
+var jqueryCalc = new project("jQuery Calculator", "assets/Images/jqueryCalculator.jpg", "https://github.com/AlexHarris-CharlotteCodeCamp/jQuery-Calculator");
+
+var projectObjectArray = [jqueryCalc, pong, spaceInvaders];
+// Activate click for right carousel button
 carouselButtonRight.addEventListener("click", function(){
     if(start < 2) {
         start++;
-        imageCarousel.setAttribute("src", imageArray[start]);
+        imageCarousel.setAttribute("src", projectObjectArray[start].image);
+        projectLink.setAttribute("href", projectObjectArray[start].link);
     } else {
         start = 0;
-        imageCarousel.setAttribute("src", imageArray[start]);
+        imageCarousel.setAttribute("src", projectObjectArray[start].image);
+        projectLink.setAttribute("href", projectObjectArray[start].link);
     }
 })
-
+// Activate click for left carousel button
 carouselButtonLeft.addEventListener("click", function(){
     if(start === 0) {
-        start = imageArray.length - 1;
-        imageCarousel.setAttribute("src", imageArray[start]);
+        start = projectObjectArray.length - 1;
+        imageCarousel.setAttribute("src", projectObjectArray[start].image);
+        projectLink.setAttribute("href", projectObjectArray[start].link);
     } else {
         start--;
-        imageCarousel.setAttribute("src", imageArray[start]);
+        imageCarousel.setAttribute("src", projectObjectArray[start].image);
+        projectLink.setAttribute("href", projectObjectArray[start].link);
     }
 })
